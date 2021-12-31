ffead044644b47982b0f98b5df028f0abc081f8a
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-12-30 19:56:16 -0500

    Add axios request in useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('')
+
+    useEffect(() => {
+        const search = async () => {
+            await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            })
+        }
+        search()
+    }, [term])
 
     return (
         <div>
@@ -10,7 +26,7 @@ const Search = () => {
                     <label>Enter Search Term</label>
                     <input 
                         value={term}
-                        onChange={e => setTerm(e.target.value)}
+                        onChange={(e) => setTerm(e.target.value)}
                         className='input' 
                     />
                 </div>
