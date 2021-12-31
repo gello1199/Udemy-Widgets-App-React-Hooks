8d7f213e7f708d7f755af4ce85bfb544794c5d17
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-12-30 20:31:14 -0500

    Add href to button

--- src/components/Search.js
+++ src/components/Search.js
@@ -25,12 +25,20 @@ const Search = () => {
 
     const renderedResults = results.map((result => {
         return (
-            <div className='item'>
+            <div key={result.pageid} className='item'>
+                <div className='right floated content'>
+                    <a 
+                        className='ui button'
+                        href={`http://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
+                </div>
                 <div className='content'>
                     <div className='header'>
                         {result.title}
                     </div>
-                        {result.snippet}
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
                 </div>
             </div>
         ) 
