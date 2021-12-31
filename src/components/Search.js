a4b5c3e811ae7f9085fd255f7a1d9269df1febd9
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-12-30 20:13:17 -0500

    Add map over results and display to DOM

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,21 +3,38 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('')
+    const [results, setResults] = useState([])
 
     useEffect(() => {
         const search = async () => {
-            await axios.get('https://en.wikipedia.org/w/api.php', {
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
                     action: 'query',
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term
+                    srsearch: term,
                 }
             })
+            setResults(data.query.search)
         }
-        search()
+        if(term) {
+            search()
+        } 
     }, [term])
+
+    const renderedResults = results.map((result => {
+        return (
+            <div className='item'>
+                <div className='content'>
+                    <div className='header'>
+                        {result.title}
+                    </div>
+                        {result.snippet}
+                </div>
+            </div>
+        ) 
+    }))
 
     return (
         <div>
@@ -30,6 +47,9 @@ const Search = () => {
                         className='input' 
                     />
                 </div>
+            </div>
+            <div className='ui celled list'>
+                {renderedResults}
             </div>
         </div>
     );
