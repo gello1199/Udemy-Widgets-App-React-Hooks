6431a0523e374e286ca0a6f3aed25996da76ffac
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-03 11:46:06 -0500

    Refactor to use debouncedTerm to debug

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,37 +3,34 @@ import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('programming')
+    const [debouncedTerm, setDebouncedTerm] = useState(term)
     const [results, setResults] = useState([])
 
-    useEffect(() => {
-        const search = async () => {
-            const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
-                params: {
-                    action: 'query',
-                    list: 'search',
-                    origin: '*',
-                    format: 'json',
-                    srsearch: term,
-                }
-            })
-            setResults(data.query.search)
-        }
+   useEffect(() => {
+    const timerId = setTimeout(() => {
+        setDebouncedTerm(term)
+    }, 1000)
 
-        if (term && !results.length) {
-            search()
-        } else {
-            const timoutId = setTimeout(() => {
-                if(term) {
-                    search()
-                } 
-            }, 1000)
-    
-            return () => {
-                clearTimeout(timoutId)
+    return () => {
+        clearTimeout(timerId)
+    }
+   }, [term])
+
+   useEffect(() => {
+    const search = async () => {
+        const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+            params: {
+                action: 'query',
+                list: 'search',
+                origin: '*',
+                format: 'json',
+                srsearch: debouncedTerm,
             }
-        }
-      
-    }, [term])
+        })
+        setResults(data.query.search)
+    }
+        search()
+   }, [debouncedTerm])
 
     const renderedResults = results.map((result => {
         return (
