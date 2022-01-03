f6df5a59307f60ce6017f172792726877bf6fe11
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-03 14:43:51 -0500

    Add onClick to change state of selected item

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Dropdown from "./components/Dropdown";
 import Search from "./components/Search";
@@ -34,11 +34,16 @@ const items = [
     ]
 
 export default () => {
+    const [selected, setSelected] = useState(options[0])
     return (
         <div>
         {/* <Accordion items={items} /> */}
         {/* <Search /> */}
-        <Dropdown options={options} />
+        <Dropdown 
+            selected={selected} 
+            onSelectedChange={setSelected}
+            options={options}  
+            />
         </div>
     )
 }
