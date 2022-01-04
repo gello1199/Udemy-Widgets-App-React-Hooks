32757c3c40b0daec4031f70054bf62a497845ec7
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-04 18:32:56 -0500

    Import Convert component to Translate component

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,6 +1,11 @@
 import React, {useState, useEffect} from 'react';
 
-const Convert = () => {
+const Convert = ({ language, text }) => {
+
+    useEffect(() => {
+        console.log("New language or text")
+    }, [language, text])
+
     return (
         <div>
             
--- src/components/Translate.js
+++ src/components/Translate.js
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
     {
@@ -45,6 +46,9 @@ const Translate = () => {
             onSelectedChange={setLanguage}
             options={options} 
         />
+        <hr />
+        <h3 className="ui header">Output</h3>
+        <Convert />
     </div>
     );
 }
