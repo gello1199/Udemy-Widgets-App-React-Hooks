9c43975c49bb8017e856ab268fe4ea33ada51871
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-04 20:48:25 -0500

    Make an api request with debouncedText

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -20,7 +20,7 @@ const Convert = ({ language, text }) => {
         const doTranslation = async () => {
             const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
                 params: {
-                    q: text,
+                    q: debouncedText,
                     target: language.value,
                     key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
                 }
@@ -29,7 +29,7 @@ const Convert = ({ language, text }) => {
         }   
 
         doTranslation()
-    }, [language, text])
+    }, [language, debouncedText])
 
     return (
         <div>
